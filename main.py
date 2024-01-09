8971ae4247d2e1e5398fc944775ce25453547f9f
silfhg <151051387+silfhg@users.noreply.github.com>
2024-01-09 23:42:08 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+from PyQt5.QtWidgets import QApplication
+
+from window import Window
+
+import sys
+
+
+if __name__ == "__main__":
+    app: QApplication(sys.argv)
+    window: Window = Window()
+    window.show()
+    sys.exit(app.exec())
